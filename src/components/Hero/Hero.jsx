cc7a9dc9794cb21b3802dfba8fdedc781830d7bd
keshala <senuu08@gmail.com>
2025-06-15 11:04:59 +0530

    Update Hero component with new personal details and image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,17 +7,18 @@ export const Hero = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Ada</h1>
+        <h1 className={styles.title}>Hi, I'm Keshala senarath</h1>
         <p className={styles.description}>
-          I'm a student at institute of technology, University of Moraratuwa. I'm a full-stack developer with 5 years of experience using React and
-          NodeJS. Reach out if you'd like to learn more!
+          I'm a student at institute of technology, University of Moratuwa.
+          I'm a full-stack developer with 2 years of experience using React and
+          NodeJS.Reach out if you'd like to learn more!
         </p>
-        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+        <a href="mailto:senuu08@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src="https://i.postimg.cc/ZRPWyrkq/Whats-App-Image-2025-06-15-at-10-27-21-623466cb.jpg"
         alt="Hero image of me"
         className={styles.heroImg}
       />
